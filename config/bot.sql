ff4411c964b6b779990cd4a926edf56df88faebc
肥羊 <youshandefeiyang@gmail.com>
2023-09-08 02:38:26 +0800

    Add files via upload

--- config/bot.sql
+++ config/bot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost
--- 生成日期： 2023-09-02 18:23:49
+-- 生成日期： 2023-09-08 02:36:01
 -- 服务器版本： 8.0.24
 -- PHP 版本： 8.1.12
 
@@ -36,7 +36,8 @@ CREATE TABLE `groupinfo` (
   `keyWordsflag` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'notallow',
   `deleteKeywordFlag` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'notdelete',
   `settingTimeStamp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `intoGroupWelcomeWords` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
+  `intoGroupWelcomeWords` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `ChannelSpammersWhiteList` longtext COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -56,6 +57,7 @@ ALTER TABLE `groupinfo`
 ALTER TABLE `groupinfo` ADD FULLTEXT KEY `ownerAndAnonymousAdmins` (`ownerAndAnonymousAdmins`);
 ALTER TABLE `groupinfo` ADD FULLTEXT KEY `intoGroupWelcomeWords` (`intoGroupWelcomeWords`);
 ALTER TABLE `groupinfo` ADD FULLTEXT KEY `keyWords` (`keyWords`);
+ALTER TABLE `groupinfo` ADD FULLTEXT KEY `ChannelSpammersWhiteList` (`ChannelSpammersWhiteList`);
 
 --
 -- 在导出的表使用AUTO_INCREMENT
