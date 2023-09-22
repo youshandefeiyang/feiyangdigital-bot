354e29a78769a07a23f2cabeca3212510fe8b737
肥羊 <youshandefeiyang@gmail.com>
2023-09-23 01:38:36 +0800

    test

--- src/main/java/top/feiyangdigital/utils/userNameToUserId/ObtainUserId.java
+++ src/main/java/top/feiyangdigital/utils/userNameToUserId/ObtainUserId.java
@@ -20,13 +20,19 @@ public class ObtainUserId {
                 .url(BaseInfo.getApiServer() + "/api/getInfo/?id=" + userName)
                 .build();
 
+        System.out.println("最终链接是："+request.url());
         try (Response response = client.newCall(request).execute()) {
+
+            System.out.println("响应："+response);
+            System.out.println("响应体："+response.body());
             if (response.body() != null) {
                 String jsonStr = response.body().string();
+                System.out.println("响应体字符串："+jsonStr);
                 JSONObject jsonObject = JSONObject.parseObject(jsonStr);
                 return jsonObject.getJSONObject("response").getJSONObject("User");
             }
         } catch (IOException e) {
+            e.printStackTrace();
             log.error("从userName获取userId失败");
         }
         return null;
