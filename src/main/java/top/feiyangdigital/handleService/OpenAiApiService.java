27b3445988197a1db20e8dc0e9f9a3a6a2d48456
肥羊 <youshandefeiyang@gmail.com>
2023-10-01 21:39:00 -0500

    Add files via upload

--- src/main/java/top/feiyangdigital/handleService/OpenAiApiService.java
+++ src/main/java/top/feiyangdigital/handleService/OpenAiApiService.java
@@ -28,7 +28,7 @@ public class OpenAiApiService {
         String content = "You are a chat app group administrator and spam detector,your\n" +
                 "job is to determine whether a user's speech is spam advertising based on the content of their statements,\n" +
                 "以下是我们专门关注的垃圾广告特征：\n" +
-                "- 使用以下关键词：区块链、黑产、赌博、色情、金融、诈骗、个人隐私信息交易，虚拟货币兑换；\n" +
+                "- 使用以下关键词：区块链、黑产、灰产、赌博、色情、金融、诈骗、个人隐私信息交易，虚拟货币交易或兑换；\n" +
                 "- 提供或引导用户使用非法服务或进行非法活动，如包含虚假支付机构或银行卡信息、诱导用户加入群组、点击链接或参与虚假活动、涉及非法支付、赌博、贩卖、引流推广、禁止物品、色情内容等。\n" +
                 "- 使用视觉混淆手段：在内容中插入符号、谐音字、特殊字符、emoji等；\n"+
                 "- 如果用户分享的是知名和正规网站，如：github.com、google.com、youtube.com、twitter.com之类，应该谨慎判定为广告。\n"+
